f5d0ca43cc811655feb50a6f47be8370eb547353
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-04-23 16:11:23 -0400

    Define the C struct on the Rust side

--- src/zopfli/lz77.rs
+++ src/zopfli/lz77.rs
@@ -1,12 +1,41 @@
-use libc::{size_t, c_ushort};
+use libc::{size_t, c_ushort, c_uchar};
 
-pub enum ZopfliLZ77Store {}
+// Comment from C:
+// Stores lit/length and dist pairs for LZ77.
+// Parameter litlens: Contains the literal symbols or length values.
+// Parameter dists: Contains the distances. A value is 0 to indicate that there is
+// no dist and the corresponding litlens value is a literal instead of a length.
+// Parameter size: The size of both the litlens and dists arrays.
+// The memory can best be managed by using ZopfliInitLZ77Store to initialize it,
+// ZopfliCleanLZ77Store to destroy it, and ZopfliStoreLitLenDist to append values.
+
+#[repr(C)]
+pub struct ZopfliLZ77Store {
+  litlens: *mut c_ushort,  /* Lit or len. */
+  dists: *mut c_ushort,  /* If 0: indicates literal in corresponding litlens,
+      if > 0: length in corresponding litlens, this is the distance. */
+  size: size_t,
+
+  data: *mut c_uchar,  /* original data */
+  pos: *mut size_t,  /* position in data where this LZ77 command begins */
+
+  ll_symbol: *mut c_ushort,
+  d_symbol: *mut c_ushort,
+
+  /* Cumulative histograms wrapping around per chunk. Each chunk has the amount
+  of distinct symbols as length, so using 1 value per LZ77 symbol, we have a
+  precise histogram at every N symbols, and the rest can be calculated by
+  looping through the actual symbols of this chunk. */
+  ll_counts: *mut size_t,
+  d_counts: *mut size_t,
+}
 
 /// Appends the length and distance to the LZ77 arrays of the ZopfliLZ77Store.
 /// Context must be a ZopfliLZ77Store*.
 #[no_mangle]
 #[allow(non_snake_case)]
-pub extern fn ZopfliStoreLitLenDist(length: c_ushort, dist: c_ushort, pos: size_t, store: *mut ZopfliLZ77Store) {
+pub extern fn ZopfliStoreLitLenDist(length: c_ushort, dist: c_ushort, pos: size_t, store: &mut ZopfliLZ77Store) {
+    let origsize = store.size;
   // size_t i;
   // /* Needed for using ZOPFLI_APPEND_DATA multiple times. */
   // size_t origsize = store->size;
