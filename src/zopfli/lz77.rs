cb3addd2e2bf6dce60be7a412914acbb444871c8
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-04-25 20:43:36 -0400

    Move functions to squeeze to match C structure

--- src/zopfli/lz77.rs
+++ src/zopfli/lz77.rs
@@ -1,8 +1,8 @@
 use std::slice;
 
-use libc::{size_t, c_ushort, c_uchar, c_int, c_void, c_uint, c_double};
+use libc::{size_t, c_ushort, c_uchar, c_int};
 
-use util::{ZopfliGetLengthSymbol, ZopfliGetLengthExtraBits, ZopfliGetDistSymbol, ZopfliGetDistExtraBits};
+use util::{ZopfliGetLengthSymbol, ZopfliGetDistSymbol};
 
 const ZOPFLI_NUM_LL: size_t = 288;
 const ZOPFLI_NUM_D: size_t = 32;
@@ -191,31 +191,4 @@ pub extern fn lz77_store_result(ptr: *mut Lz77Store, store: &mut ZopfliLZ77Store
     store.d_symbol = lz77.d_symbol.as_mut_ptr();
     store.ll_counts = lz77.ll_counts.as_mut_ptr();
     store.d_counts = lz77.d_counts.as_mut_ptr();
-}
-
-#[no_mangle]
-#[allow(non_snake_case)]
-/// Cost model which should exactly match fixed tree.
-/// type: CostModelFun
-pub extern fn GetCostFixed(litlen: c_uint, dist: c_uint, _unused: c_void) -> c_double {
-    let result = if dist == 0 {
-        if litlen <= 143 {
-            8
-        } else {
-            9
-        }
-    } else {
-        let dbits = ZopfliGetDistExtraBits(dist as c_int);
-        let lbits = ZopfliGetLengthExtraBits(litlen as c_int);
-        let lsym = ZopfliGetLengthSymbol(litlen as c_int);
-        let mut cost = 0;
-        if lsym <= 279 {
-            cost += 7;
-        } else {
-            cost += 8;
-        }
-        cost += 5;  // Every dist symbol has length 5.
-        cost + dbits + lbits
-    };
-    result as c_double
 }
