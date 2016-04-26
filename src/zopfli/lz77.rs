6672a828a9b145ab0d84d177b5c43c3021ce25b8
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-04-25 20:02:32 -0400

    Move GetCostFixed to rust

--- src/zopfli/lz77.rs
+++ src/zopfli/lz77.rs
@@ -1,6 +1,6 @@
 use std::slice;
 
-use libc::{size_t, c_ushort, c_uchar, c_int};
+use libc::{size_t, c_ushort, c_uchar, c_int, c_void, c_uint, c_double};
 
 const ZOPFLI_NUM_LL: size_t = 288;
 const ZOPFLI_NUM_D: size_t = 32;
@@ -315,4 +315,31 @@ pub extern fn ZopfliGetDistExtraBits(dist: c_int) -> c_int {
             _ => 13,
         }
     }
+}
+
+#[no_mangle]
+#[allow(non_snake_case)]
+/// Cost model which should exactly match fixed tree.
+/// type: CostModelFun
+pub extern fn GetCostFixed(litlen: c_uint, dist: c_uint, _unused: c_void) -> c_double {
+    let result = if dist == 0 {
+        if litlen <= 143 {
+            8
+        } else {
+            9
+        }
+    } else {
+        let dbits = ZopfliGetDistExtraBits(dist as c_int);
+        let lbits = ZopfliGetLengthExtraBits(litlen as c_int);
+        let lsym = ZopfliGetLengthSymbol(litlen as c_int);
+        let mut cost = 0;
+        if lsym <= 279 {
+            cost += 7;
+        } else {
+            cost += 8;
+        }
+        cost += 5;  // Every dist symbol has length 5.
+        cost + dbits + lbits
+    };
+    result as c_double
 }
