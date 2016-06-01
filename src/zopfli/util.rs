8b6e93e34c7b16fbd8afa8deeeb30cc3eed99d0b
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-05-31 22:06:38 -0400

    Add ZOPFLI_MASTER_BLOCK_SIZE to rust

--- src/zopfli/util.rs
+++ src/zopfli/util.rs
@@ -10,3 +10,10 @@ pub const ZOPFLI_MIN_MATCH: size_t = 3;
 pub const ZOPFLI_CACHE_LENGTH: size_t = 8;
 pub const ZOPFLI_MAX_CHAIN_HITS: size_t = 8192;
 pub const ZOPFLI_LARGE_FLOAT: c_double = 1E30;
+
+/// A block structure of huge, non-smart, blocks to divide the input into, to allow
+/// operating on huge files without exceeding memory, such as the 1GB wiki9 corpus.
+/// The whole compression algorithm, including the smarter block splitting, will
+/// be executed independently on each huge block.
+/// Dividing into huge blocks hurts compression, but not much relative to the size.
+pub const ZOPFLI_MASTER_BLOCK_SIZE: size_t = 1000000;
